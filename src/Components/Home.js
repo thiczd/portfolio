d1661b1ce1f93379c109893008b22f64aaebc653
thibault <breakthegreat@gmail.com>
2024-07-09 14:37:52 -0400

    small css tweaks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,6 @@ import htmlImage from "../images/html-5.png";
 import jsImage from "../images/js.png";
 import mysqlImage from "../images/mysql.png";
 import pythonImage from "../images/python.png";
-import structureImage from "../images/structure.png";
 import reactImage from "../images/react.png";
 const Home = () => {
   return (
@@ -14,37 +13,37 @@ const Home = () => {
       <div className="home">
         <h1 className="title">Homepage</h1>
         <div className="description">Welcome to my portfolio website.</div>
-        <div className="portfolio">
-          <div className="grid">
-            <div className="grid-html-css">
-              <img src={htmlImage} alt="HTML Logo" />
-              <img src={cssImage} alt="CSS Logo" />
+        <div className="skills">
+          <h1>Key Skills</h1>
+
+          <div className="portfolio">
+            <div className="grid-box">
+              <div className="grid-html-css">
+                <img src={htmlImage} alt="HTML Logo" />
+                <img src={cssImage} alt="CSS Logo" />
+              </div>
+              <span>HTML/CSS</span>
             </div>
-            <span>HTML/CSS</span>
-          </div>
-          <div className="grid">
-            <img src={jsImage} alt="JavaScript Logo" />
-            <span>JavaScript</span>
-          </div>
-          <div className="grid">
-            <img src={reactImage} alt="React Logo" />
-            <span>React</span>
-          </div>
-          <div className="grid">
-            <img src={cImage} alt="C++ Logo" />
-            <span>C++</span>
-          </div>
-          <div className="grid">
-            <img src={pythonImage} alt="Python Logo" />
-            <span>Python</span>
-          </div>
-          <div className="grid">
-            <img src={mysqlImage} alt="MySQL Logo" />
-            <span>MySQL</span>
-          </div>
-          <div className="grid">
-            <img src={structureImage} alt="Structure Logo" />
-            <span>Structure</span>
+            <div className="grid-box">
+              <img src={jsImage} alt="JavaScript Logo" />
+              <span>JavaScript</span>
+            </div>
+            <div className="grid-box">
+              <img src={reactImage} alt="React Logo" />
+              <span>React</span>
+            </div>
+            <div className="grid-box">
+              <img src={cImage} alt="C++ Logo" />
+              <span>C++</span>
+            </div>
+            <div className="grid-box">
+              <img src={pythonImage} alt="Python Logo" />
+              <span>Python</span>
+            </div>
+            <div className="grid-box">
+              <img src={mysqlImage} alt="MySQL Logo" />
+              <span>MySQL</span>
+            </div>
           </div>
         </div>
       </div>
