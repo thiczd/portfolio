fa037302c2a89195419f78988154ad847189a986
thibault <breakthegreat@gmail.com>
2024-07-09 14:21:00 -0400

    added grid layout and more image for the grid

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "../css/home.css";
+import cImage from "../images/c-.png";
+import cssImage from "../images/css-3.png";
+import htmlImage from "../images/html-5.png";
+import jsImage from "../images/js.png";
+import mysqlImage from "../images/mysql.png";
+import pythonImage from "../images/python.png";
+import structureImage from "../images/structure.png";
+import reactImage from "../images/react.png";
 const Home = () => {
   return (
     <>
@@ -7,12 +15,37 @@ const Home = () => {
         <h1 className="title">Homepage</h1>
         <div className="description">Welcome to my portfolio website.</div>
         <div className="portfolio">
-          <div className="grid">HTML/CSS</div>
-          <div className="grid">JavaScript</div>
-          <div className="grid">React</div>
-          <div className="grid">C++</div>
-          <div className="grid">Python</div>
-          <div className="grid">MySQL</div>
+          <div className="grid">
+            <div className="grid-html-css">
+              <img src={htmlImage} alt="HTML Logo" />
+              <img src={cssImage} alt="CSS Logo" />
+            </div>
+            <span>HTML/CSS</span>
+          </div>
+          <div className="grid">
+            <img src={jsImage} alt="JavaScript Logo" />
+            <span>JavaScript</span>
+          </div>
+          <div className="grid">
+            <img src={reactImage} alt="React Logo" />
+            <span>React</span>
+          </div>
+          <div className="grid">
+            <img src={cImage} alt="C++ Logo" />
+            <span>C++</span>
+          </div>
+          <div className="grid">
+            <img src={pythonImage} alt="Python Logo" />
+            <span>Python</span>
+          </div>
+          <div className="grid">
+            <img src={mysqlImage} alt="MySQL Logo" />
+            <span>MySQL</span>
+          </div>
+          <div className="grid">
+            <img src={structureImage} alt="Structure Logo" />
+            <span>Structure</span>
+          </div>
         </div>
       </div>
     </>
