1e3a4d0f617281f3e0ae830d4c35004ac952fab3
thibault <breakthegreat@gmail.com>
2024-07-15 15:59:01 -0400

    added changes on project card

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "../css/about.css";
+import { motion } from "framer-motion";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-me-main">
+      <div className="about-me">
+        <motion.h1
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 1,
+            type: "spring",
+            stiffness: 150,
+            damping: 7,
+          }}
+        >
+          More about Me
+        </motion.h1>
+        <p>
+          I am currently pursuing a bachelor's degree in Computer Science at New
+          Jersey Institute of Technology (NJIT), with an expected graduation
+          date of 2024. I have developed a strong passion for computer
+          programming and data science, acquiring skills in a range of
+          programming languages and technologies including SQL, GitHub,
+          JavaScript, Java, C++, C, Python, Numpy, Matplotlib, PyTorch, Jax, and
+          various machine learning algorithms and techniques. I have applied
+          these skills in several projects, notably creating a web application
+          using HTML/CSS, JavaScript (jQuery), and PHP. I am actively seeking
+          job or internship opportunities that will allow me to leverage and
+          further develop my skills, and contribute to the advancement and
+          innovation in the field.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,33 +3,43 @@ import "../css/project.css";
 import { motion } from "framer-motion";
 
 const Project = () => {
-  const [isOpen, setIsOpen] = useState(null);
-
-  const toggleCard = (index) => {
-    setIsOpen(isOpen === index ? null : index);
-  };
   const projects = [
     {
-      title: "Some stuff ",
+      title: " Project 1 🚀",
       description:
         "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      title: "Some stuff ",
+      title: " Project 2 ⭐",
       description:
         "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
     {
-      title: "Some stuff ",
+      title: " Project 3 💻",
       description:
         "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
   ];
+  const [isOpen, setIsOpen] = useState(Array(projects.length).fill(false));
+
+  const toggleCard = (index) => {
+    setIsOpen((prev) => prev.map((item, i) => (i === index ? !item : item)));
+  };
+
+  const x_screen = -10;
+  const y_screen = -10;
 
   return (
     <>
       <div className="main-project">
-        <h1 className="title-projects-section"> My Current Projects</h1>
+        <motion.h1
+          className="title-projects-section"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.7 }}
+        >
+          My Current Projects
+        </motion.h1>
         <motion.div
           className="grid-project"
           transition={{ layout: { duration: 1, type: "spring" } }}
@@ -38,16 +48,21 @@ const Project = () => {
           {projects.map(({ description, title }, index) => (
             <motion.div
               key={index}
-              transition={{ layout: { duration: 1, type: "spring" } }}
+              transition={{
+                layout: { duration: 1, type: "spring" },
+                duration: 1,
+              }}
               layout
               onClick={() => {
                 toggleCard(index);
               }}
               className="grid-box-project"
               style={{ borderRadius: "12px" }}
+              initial={{ x: x_screen * index, y: y_screen * index }}
+              animate={{ x: 0, y: 0 }}
             >
               <motion.h2 layout="position">{title}</motion.h2>
-              {isOpen === index && (
+              {isOpen[index] && (
                 <motion.div
                   layout="position"
                   initial={{ opacity: 0 }}
