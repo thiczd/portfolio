b77697cb7edcfd6dff838a11f20fefc49b2af120
thibault <breakthegreat@gmail.com>
2024-07-16 14:32:52 -0400

    added title animation about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,41 +1,36 @@
 import React from "react";
 import "../css/about.css";
 import { motion } from "framer-motion";
-
+import AboutTitle from "./AboutTitle";
 const About = () => {
   return (
     <div className="about-me-main">
       <div className="about-me">
-        <motion.h1
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
+        <AboutTitle text="More about Me 👋"></AboutTitle>
+        <motion.p
+          initial={{ opacity: 0, scale: 0.75 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{
             duration: 1,
-            type: "spring",
-            stiffness: 150,
-            damping: 7,
           }}
         >
-          More about Me 👋
-          <hr></hr>
-        </motion.h1>
-
-        <p>
           I am currently pursuing a bachelor's degree in Computer Science at New
           Jersey Institute of Technology (NJIT), with an expected graduation
-          date of Dec. 2024. <br></br>I have developed a strong passion for
-          computer programming, acquiring skills in a range of programming
-          languages and technologies including SQL, GitHub, JavaScript, Java,
-          C++, C, Python, Numpy, Matplotlib, PyTorch, Jax. My main focus is Web
-          Devlopment as it is in my opinion a critical role in 2024. I am
-          currently learning ReactJs, Node.js, Express and MongoDB to fine tune
-          my full stack developper skills
+          date of Dec. 2024. <br></br>
+          <br></br>I have developed a strong passion for computer programming,
+          acquiring skills in a range of programming languages and technologies
+          including SQL, GitHub, JavaScript, Java, C++, C, Python, Numpy,
+          Matplotlib, PyTorch, Jax. My main focus is Web Devlopment as it is in
+          my opinion a critical role in 2024. I am currently learning ReactJs,
+          Node.js, Express and MongoDB to fine tune my full stack developper
+          skills
+          <br></br>
           <br></br> I have applied these skills in several projects, notably
           creating a web application using HTML/CSS, JavaScript (jQuery), PHP,
           ReactJs. I am actively seeking job or internship opportunities that
           will allow me to leverage and further develop my skills, and
           contribute to the advancement and innovation in the field.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
