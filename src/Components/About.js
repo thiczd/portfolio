496d55efb224cc592d2d626a438f177bc9abd1ba
breakthegreat <bloodmodii@gmail.com>
2025-01-29 14:27:21 -0600

    added fix to about and keyskills

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="about-me-main">
       <div className="about-me">
-        <AboutTitle text="More about Me 👋"></AboutTitle>
+        <AboutTitle text="More about me 👋"></AboutTitle>
 
         <motion.hr
           initial={{ scale: 0 }}
@@ -27,24 +27,33 @@ const About = () => {
           }}
           className="about-me-description"
         >
-          I am currently pursuing a bachelor's degree in Computer Science at New
-          Jersey Institute of Technology (NJIT), with an expected graduation
-          date of Dec. 2024.
+          I recently graduated from the New Jersey Institute of Technology with
+          a Bachelor's in Computer Science
           <br></br>
           <br></br>I have developed a strong passion for computer programming,
           acquiring skills in a range of programming languages and technologies
-          including SQL, GitHub, JavaScript, Java, C++, C, Python, Numpy,
-          Matplotlib, PyTorch, Jax.
-          <br></br>
+          including: <br></br>
+          <ul>
+            <li>C </li>
+            <li>C++ </li>
+            <li>Java </li>
+            <li>Python </li>
+          </ul>
           My main focus is Web Development as it is in my opinion a critical
           role in 2024. I am currently learning ReactJs, Node.js, Express and
-          MongoDB to fine tune my full stack developer skills. I have applied
-          these skills in several projects, notably creating a web application
-          using HTML/CSS, JavaScript (jQuery), PHP, ReactJs.
-          <br></br>
-          <br></br>I am actively seeking job or internship opportunities that
-          will allow me to leverage and further develop my skills, and
-          contribute to the advancement and innovation in the field.
+          MongoDB to fine tune my full stack developer skills.
+          <br></br>I have applied these skills in several projects, notably
+          creating a web application using:
+          <ul>
+            <li> HTML/CSS </li>
+            <li> Javascript </li>
+            <li> PHP </li>
+            <li> Coldfusion </li>
+            <li> ReactJs </li>
+            <li> ExpressJs </li>
+          </ul>
+          I am actively seeking full time employment opportunities in
+          software/web development
         </motion.div>
       </div>
     </div>
